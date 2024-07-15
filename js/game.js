9256610e8770a4e403cbeb837718a5a2c4d61ddc
Vanski3 <122392034+Vanski3@users.noreply.github.com>
2024-07-15 12:48:54 +0200

    world, movables, chicken, character, game, index initations

--- js/game.js
+++ js/game.js
@@ -1,15 +1,9 @@
 let canvas;
-let ctx;
-let character = new Character();
-let enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken()
-];
+let world; 
 
 function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-
-    console.log('my character is', character);
+    world = new World(canvas);
+    
+    console.log('my character is', world.character);
 }
